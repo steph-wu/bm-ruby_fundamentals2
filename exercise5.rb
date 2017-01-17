add05b50e0b9c77267588715097c6c51301f009e
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-01-17 12:29:58 -0500

    Returns result of conversion in full sentence

--- exercise5.rb
+++ exercise5.rb
@@ -1,7 +1,8 @@
 def conversion(f)
-  puts (f - 32) * 5 / 9
+  celsius = (f - 32) * 5 / 9
+  puts "#{f} degrees Fahrenheit is #{celsius} degrees Celsius."
 end
 
 puts "Enter a temperature in Fahrenheit:"
-x = gets.chomp.to_f
+x = gets.chomp.to_i
 conversion(x)
